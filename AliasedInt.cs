5875e6569da21124a66b8ff1d51ea9a92527f5ef
Jon Hanna <jon@hackcraft.net>
2012-01-02 23:01:24 +0000

    AliasedInt is useful. Let's flesh it out, and make it public.

---
+++ AliasedInt.cs
@@ -0,0 +1,70 @@
+﻿// © 2012 Jon Hanna.
+// This source code is licensed under the EUPL, Version 1.1 only (the “Licence”).
+// You may not use, modify or distribute this work except in compliance with the Licence.
+// You may obtain a copy of the Licence at:
+// <http://joinup.ec.europa.eu/software/page/eupl/licence-eupl>
+// A copy is also distributed with this source code.
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// Licence is distributed on an “AS IS” basis, without warranties or conditions of any kind.
+
+using System;
+using System.Threading;
+
+namespace HackCraft.LockFree
+{
+	/// <summary>A simple means to share an atomically-maintained count between objects.</summary>
+	public sealed class AliasedInt
+	{
+	    private int _value;
+	    /// <summary>Creates a new AliasedInt with a value of zero.</summary>
+	    public AliasedInt(){}
+	    /// <summary>Creates a new AliasedInt.</summary>
+	    /// <param name="value">The initial value of the object.</param>
+	    public AliasedInt(int value)
+	    {
+	        _value = value;
+	    }
+	    /// <summary>Returns the value of the AliasedInt.</summary>
+	    public int Value
+	    {
+	        get { return _value; }
+	    }
+	    /// <summary>Returns the value of the AliasedInt.</summary>
+	    /// <param name="ri">The AliasedInt to cast.</param>
+	    /// <returns>An integer of the same value as the AliasedInt.</returns>
+	    public static implicit operator int(AliasedInt ri)
+	    {
+	        return ri._value;
+	    }
+	    /// <summary>Atomically increment the value of the AliasedInt by one.</summary>
+	    /// <returns>The new value.</returns>
+	    public int Increment()
+	    {
+	        return Interlocked.Increment(ref _value);
+	    }
+	    /// <summary>Atomically decrement the value of the AliasedInt by one.</summary>
+	    /// <returns>The new value.</returns>
+	    public int Decrement()
+	    {
+	        return Interlocked.Decrement(ref _value);
+	    }
+	    /// <summary>Atomically add a value to the AliasedInt.</summary>
+	    /// <returns>The new value.</returns>
+	    public int Add(int addend)
+	    {
+	        return Interlocked.Add(ref _value, addend);
+	    }
+	    /// <summary>Atomically replace the value of the AliasedInt, returning the previous value.</summary>
+	    /// <returns>The old value.</returns>
+	    public int Exchange(int value)
+	    {
+	        return Interlocked.Exchange(ref _value, value);
+	    }
+	    /// <summary>Atomically subtract a value from the AliasedInt.</summary>
+	    /// <returns>The new value.</returns>
+	    public int Subtract(int subtrahend)
+	    {
+	        return Interlocked.Add(ref _value, -subtrahend);
+	    }
+	}
+}
--- InternalHelpers.cs
+++ InternalHelpers.cs
@@ -9,27 +9,10 @@
 
 using System;
 using System.Resources;
-using System.Threading;
 
 namespace HackCraft.LockFree
 {
-    //Simple means to share an atomically-maintained count between objects.
-    internal sealed class AliasedInt
-    {
-        private int _value;
-        public static implicit operator int(AliasedInt ri)
-        {
-            return ri._value;
-        }
-        public int Increment()
-        {
-            return Interlocked.Increment(ref _value);
-        }
-        public int Decrement()
-        {
-            return Interlocked.Decrement(ref _value);
-        }
-    }
+
     internal sealed class SinglyLinkedNode<T>
     {
         public T Item;
