f7abef88a01d49127b5eb86c974122da3f07de22
Jon Hanna <jon@hackcraft.net>
2012-01-05 16:27:33 +0000

    Catch null collection to queue constructor.

--- LLQueue.cs
+++ LLQueue.cs
@@ -49,6 +49,8 @@ namespace Ariadne.Collections
         public LLQueue(IEnumerable<T> collection)
             :this()
         {
+            if(collection == null)
+                throw new ArgumentNullException("collection");
             foreach(T item in collection)
                 Enqueue(item);
         }
