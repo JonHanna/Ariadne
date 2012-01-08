05a63ac37deab843dc781abd30192913440c8f3e
Jon Hanna <jon@hackcraft.net>
2012-01-08 23:11:16 +0000

    May as well add a non-generic version.

--- ReferenceEqualityComparer.cs
+++ ReferenceEqualityComparer.cs
@@ -15,16 +15,22 @@
 // (I was using this with some experimenting, and it seemed of reasonable enough general use).
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace Ariadne
 {
-    internal static class ObjectHashCodeCalculater
+    /// <summary>An implementation of <see cref="IEqualityComparer"/> which
+    /// compares for object identity (reference equality), ignoring any overrides
+    /// of <see cref="object.Equals(object)"/> and <see cref="object.GetHashCode()"/>
+    /// or any implementation of <see cref="IEquatable&lt;T>"/>.</summary>
+    /// <threadsafety static="true" instance="true"/>
+    public class ReferenceEqualityComparer : IEqualityComparer
     {
-        private static Func<object, int> rootHashCode;
-        static ObjectHashCodeCalculater()
+        protected static Func<object, int> RootHashCode;
+        static ReferenceEqualityComparer()
         {
             // Note: If moved to .NET 2.0 prior to SP 1, requires ReflectionPermission with ReflectionPermissionFlag.ReflectionEmit
             DynamicMethod dynM = new DynamicMethod(string.Empty, typeof(int), new Type[]{typeof(object)}, typeof(object));
@@ -33,11 +39,23 @@ namespace Ariadne
             ilGen.Emit(OpCodes.Call, typeof(object).GetMethod("GetHashCode"));
             ilGen.Emit(OpCodes.Ret);
             
-            rootHashCode = (Func<object, int>)dynM.CreateDelegate(typeof(Func<object, int>));
+            RootHashCode = (Func<object, int>)dynM.CreateDelegate(typeof(Func<object, int>));
         }
-        public static int GetRootHashCode(object obj)
+        /// <summary>Returns true if the two arguments are the same object, false otherwise.</summary>
+        /// <param name="x">The first item to compare.</param>
+        /// <param name="y">The second item to compare.</param>
+        /// <returns>True if they are the same object, false otherwise.</returns>
+        public bool Equals(object x, object y)
         {
-            return rootHashCode(obj);
+            return ReferenceEquals(x, y);
+        }
+        /// <summary>Returns the identity-based hash-code defined in <see cref="object"/>,
+        /// ignoring any overrides.</summary>
+        /// <param name="obj">The object to obtain a hash-code for.</param>
+        /// <returns>The hash-code.</returns>
+        public int GetHashCode(object obj)
+        {
+            return RootHashCode(obj);
         }
     }
     /// <summary>An implementation of <see cref="IEqualityComparer&lt;T>"/> which
@@ -46,7 +64,7 @@ namespace Ariadne
     /// or any implementation of <see cref="IEquatable&lt;T>"/>.</summary>
     /// <typeparam name="T">The type of objects to compare.</typeparam>
     /// <threadsafety static="true" instance="true"/>
-    public class ReferenceEqualityComparer<T> : IEqualityComparer<T> where T : class
+    public class ReferenceEqualityComparer<T> : ReferenceEqualityComparer, IEqualityComparer<T> where T : class
     {
         /// <summary>Returns true if the two arguments are the same object, false otherwise.</summary>
         /// <param name="x">The first item to compare.</param>
@@ -62,7 +80,7 @@ namespace Ariadne
         /// <returns>The hash-code.</returns>
         public int GetHashCode(T obj)
         {
-            return ObjectHashCodeCalculater.GetRootHashCode(obj);
+            return RootHashCode(obj);
         }
     }
 }
