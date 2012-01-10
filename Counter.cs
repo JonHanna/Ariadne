8e940ae370b5a05ac6a2af38083c7d15f090ca1c
Jon Hanna <jon@hackcraft.net>
2012-01-10 00:37:55 +0000

    Perf. improvements and operators.

--- Counter.cs
+++ Counter.cs
@@ -21,7 +21,14 @@ namespace Ariadne
     [StructLayout(LayoutKind.Explicit)]
     internal struct OffsetInt
     {
-        [FieldOffset(64)]
+        //We want the offset to be Cache-line-size - sizeof(int).
+        //On most chips that .NET or Mono run on at the moment, this would be 64 - 4 = 60.
+        //On Itanium, it would be 128 - 4 = 124. Other sizes are both possible and quite likely
+        //to appear in the future. We could P-Invoke and find the precise size to use (and use
+        //an array of ints with gaps decided on that basis), but that wouldn't be portable, so we
+        //just assume that 64 is the cache-line size and while we don't improve as much as we
+        //can on Itanium, we'll at least improve things.
+        [FieldOffset(60)]
         public int Num;
     }
     //about 20% slower on single-thread, much faster on multi-thread than Interlocked.Increment.
@@ -43,9 +50,21 @@ namespace Ariadne
         private readonly int mask;
         public Counter()
         {
-            int cores = CoreCount;
-            mask = cores - 1;
-            counters = new OffsetInt[cores];
+            //We won't go above 32 so that the total array size (including overhead) fits in a 4KiB page.
+            //We won't go below 16 so we've a good spread.
+            int size = EstimateCoreCount <= 4 ? 16 : 32;
+            unchecked // binary round-up
+            {
+                --size;
+                size |= (size >> 1);
+                size |= (size >> 2);
+                size |= (size >> 4);
+                size |= (size >> 8);
+                size |= (size >> 16);
+                ++size;
+            }
+            mask = size - 1;
+            counters = new OffsetInt[size];
         }
         public int Value
         {
@@ -63,11 +82,23 @@ namespace Ariadne
 	    }
         public void Increment()
         {
-            Interlocked.Increment(ref counters[(Thread.CurrentThread.ManagedThreadId) & mask].Num);
+            //We avoid different cores hitting the same counter, but don't completely prohibit it, so we
+            //still need Interlocked.
+            Interlocked.Increment(ref counters[Thread.CurrentThread.ManagedThreadId & mask].Num);
         }
         public void Decrement()
         {
-            Interlocked.Decrement(ref counters[(Thread.CurrentThread.ManagedThreadId) & mask].Num);
+            Interlocked.Decrement(ref counters[Thread.CurrentThread.ManagedThreadId & mask].Num);
+        }
+        public static Counter operator ++(Counter c)
+        {
+            c.Increment();
+            return c;
+        }
+        public static Counter operator --(Counter c)
+        {
+            c.Decrement();
+            return c;
         }
     }
 }
